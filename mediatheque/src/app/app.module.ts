6f8a0fabff760a850c2807ae4892f8fdb116c198
mxd838 <mxduran@protonmail.com>
2022-06-30 11:50:19 +0200

    initial commit

---
+++ mediatheque/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
